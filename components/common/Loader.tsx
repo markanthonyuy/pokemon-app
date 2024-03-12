3563dd57a26e20ea82c7fd927a172947d55381b9
Mac mac <macmac.uy@gmail.com>
2024-03-12 15:32:52 +0800

    make loader component dark mode friendly

--- components/common/Loader.tsx
+++ components/common/Loader.tsx
@@ -1,8 +1,16 @@
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
 import { PropsWithChildren } from 'react';
 import { ActivityIndicator } from 'react-native';
 
-export const Loader = (
-  props?: PropsWithChildren<ActivityIndicator | { color: string }>
-) => {
-  return <ActivityIndicator size="small" color="black" {...props} />;
+export const Loader = (props?: PropsWithChildren<ActivityIndicator | {}>) => {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ActivityIndicator
+      size="small"
+      color={Colors[colorScheme ?? 'light'].tint}
+      {...props}
+    />
+  );
 };
