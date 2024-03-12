97aafcd8dfeb3c8f1ee4c3accce2c7c8b6f20175
Mac mac <macmac.uy@gmail.com>
2024-03-12 15:23:33 +0800

    fix android and dark mode issues

--- components/common/Loader.tsx
+++ components/common/Loader.tsx
@@ -1,6 +1,8 @@
 import { PropsWithChildren } from 'react';
 import { ActivityIndicator } from 'react-native';
 
-export const Loader = (props?: PropsWithChildren<ActivityIndicator | {}>) => {
+export const Loader = (
+  props?: PropsWithChildren<ActivityIndicator | { color: string }>
+) => {
   return <ActivityIndicator size="small" color="black" {...props} />;
 };
